237cb4ee6d4b2e20dcfafdc353cec6d3fc8bfba9
Cliff Gor <clierozigy@gmail.com>
2019-11-29 21:32:38 +0300

    Updates

---
+++ precache-manifest.6988c10d6c5d23a4747b293475eed5d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a64d0058bbae6bac142c169355f00e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be0e1efa41cefa61b31d",
+    "url": "/static/css/2.5f095bf2.chunk.css"
+  },
+  {
+    "revision": "cdc5e9ef54e609abaadf",
+    "url": "/static/css/main.20d982b5.chunk.css"
+  },
+  {
+    "revision": "be0e1efa41cefa61b31d",
+    "url": "/static/js/2.c5bd28bf.chunk.js"
+  },
+  {
+    "revision": "cdc5e9ef54e609abaadf",
+    "url": "/static/js/main.958b5b4e.chunk.js"
+  },
+  {
+    "revision": "6beddab83ef9d13cef65",
+    "url": "/static/js/runtime~main.11e20a59.js"
+  },
+  {
+    "revision": "41e2ebe777c5c0a3ee7f21b470e2f986",
+    "url": "/static/media/404.41e2ebe7.jpg"
+  },
+  {
+    "revision": "e8453471cc387ab9c46398183e8c05bc",
+    "url": "/static/media/loader.e8453471.gif"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
